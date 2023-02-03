5f83a13703113e958e7a4561279cfeada30b44cd
gecitemre <emregecit5@gmail.com>
2023-02-03 20:46:51 +0300

    fenwick tree added

---
+++ fenwick.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+class fenwick {
+    private:
+        long *tree;
+        void add(long val, int x, int size) {
+            while (x <= size) {
+                tree[x] += val;
+                x += x & (-x);
+            }
+        }
+    public:
+        fenwick(int size, const long array[]){
+            tree = new long[size + 1];
+            memset(tree, 0, sizeof(long) * (size + 1));
+            for (int i = 1; i <= size; i++) {
+                add(array[i], i);
+            }
+        }
+        long sum(int x) {
+            long res = 0;
+            while (x >= 1) {
+                res += tree[x];
+                x -= x & (-x);
+            }
+            return res;
+        }
+        long query(int l, int r) {
+            return sum(r) - sum(l - 1);
+        }
+}
