d377f02f307023e86fb236cf664cda0bb5da0dfe
gecitemre <emregecit5@gmail.com>
2023-02-04 23:08:25 +0300

    - created headers.cpp - removed isOdd functions

---
+++ headers.cpp
@@ -0,0 +1,4 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
--- math.cpp
+++ math.cpp
@@ -3,19 +3,11 @@
 long fast_pow(long base, long exp) {
     long res = 1;
     while (exp > 0) {
-        if (isOdd(exp)) {
+        if (exp % 2) {
             res = (res * base) % MOD;
         }
         base = (base * base) % MOD;
         exp >>= 1;
     }
     return res;
-}
-
-inline bool isOdd(int i) {
-    return i & 1;
-}
-
-inline bool isOdd(long i) {
-    return i % 2;
 }
